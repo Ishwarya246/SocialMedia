e780a84c12171c1662094d01b9c1f34a21ef2f4d
Gowtham2543 <gowtham.manoharan@outlook.com>
2023-01-05 00:25:33 +0530

    DB connection established

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from werkzeug.security import generate_password_hash, check_password_hash
-import models, jwt, uuid
+from models import models
+import jwt, uuid
 
 app = Flask(__name__)
 
-#db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:1234@localhost/SocialMedia'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+models.db.init_app(app)
 
 @app.route("/")
 def home():
