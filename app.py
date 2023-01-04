9c2514746856334a0af3f07fae0d2a7fbc031023
Gowtham2543 <gowtham.manoharan@outlook.com>
2023-01-05 00:25:33 +0530

    Rendering sites

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
-from flask import Flask
+from flask import Flask, request, make_response, render_template
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import models
+from models.database import db, User
 import jwt, uuid
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:1234@localhost/SocialMedia'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-models.db.init_app(app)
+db.init_app(app)
 
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template('Test.html')
+
+@app.route("/signup")
+def signup():
+    data = request.form
+
+    name = data.get("name")
+    email = data.get("email")
+    password = data.get("password")
+
+    user = User.query.filter_by(email = email).first()
+
+    if not user:
+        user = User(public_id = str(uuid.uuid4()),
+                    name = name,
+                    email = email,
+                    password = generate_password_hash(password))
+
+        db.session.add(user)
+        db.session.commit()
+
+        return make_response('Registered Successfully')
+    else:
+        return make_response('User already exists')
---
+++ models/database.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy 
+
+db = SQLAlchemy()
+
+class User(db.Model) :
+
+    __tablename__ = "user"
+    id = db.Column(db.Integer , primary_key = True)
+    public_id  = db.Column(db.String(100) , unique = True)
+    name  = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(256))
+
+
+    def __init__(self, public_id, name , email , password) :
+        self.public_id = public_id
+        self.name =  name
+        self.email = email
+        self.password = password 
+
+    def as_dict(self) : 
+        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
